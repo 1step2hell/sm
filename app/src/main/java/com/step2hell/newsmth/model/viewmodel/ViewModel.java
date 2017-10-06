6c7064f559e47a0096b34ab6c648e176d001bf9a
Bob <40080007@qq.com>
2017-10-06 15:08:07 +0800

    add databinding

--- app/src/main/java/com/step2hell/newsmth/model/viewmodel/ViewModel.java
+++ app/src/main/java/com/step2hell/newsmth/model/viewmodel/ViewModel.java
@@ -1,9 +1,20 @@
 package com.step2hell.newsmth.model.viewmodel;
 
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.step2hell.newsmth.R;
 import com.step2hell.newsmth.model.bean.AdvBean;
 import com.step2hell.newsmth.model.datamodel.DataModel;
 
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 
 public class ViewModel {
@@ -28,4 +39,28 @@ public class ViewModel {
     public Observable<AdvBean> fetchAdv() {
         return DataModel.ADV.fetch();
     }
+
+    @BindingAdapter({"bind:adv"})
+    public static void setupAdv(final ImageView view, Observable<AdvBean> observable) {
+        observable.subscribe(new Consumer<AdvBean>() {
+            @Override
+            public void accept(@NonNull final AdvBean bean) throws Exception {
+                RequestOptions options = new RequestOptions()
+                        .fitCenter()
+                        .placeholder(R.mipmap.ic_launcher)
+                        .transform(new RoundedCorners(8));
+                Glide.with(view.getContext())
+                        .load(bean.getFile())
+                        .apply(options)
+                        .into(view);
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(view.getContext(), bean.getUrl(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/step2hell/newsmth/ui/main/AdvActivity.java
+++ app/src/main/java/com/step2hell/newsmth/ui/main/AdvActivity.java
@@ -1,86 +1,22 @@
 package com.step2hell.newsmth.ui.main;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
 import com.step2hell.newsmth.R;
-import com.step2hell.newsmth.model.bean.AdvBean;
+import com.step2hell.newsmth.databinding.ActivityAdvBinding;
 import com.step2hell.newsmth.model.viewmodel.ViewModel;
 import com.step2hell.newsmth.ui.BaseActivity;
-
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
 
 
 public class AdvActivity extends BaseActivity {
 
-    private CompositeDisposable disposable;
-
-    private ImageView advImg;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adv);
-
+        ActivityAdvBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_adv);
+        binding.setViewModel(ViewModel.getInstance());
         setupToolbar();
-
-        // Todo: 判断网络先
-        disposable = new CompositeDisposable();
-        setupViews();
-    }
-
-    private void setupViews() {
-        advImg = (ImageView) findViewById(R.id.adv);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        bind();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unBind();
-    }
-
-    private void bind() {
-        disposable.add(ViewModel.getInstance().fetchAdv().subscribe(new Consumer<AdvBean>() {
-            @Override
-            public void accept(@NonNull AdvBean advBean) throws Exception {
-                setAdvImg(advBean);
-            }
-        }));
-    }
-
-    private void unBind() {
-        disposable.clear();
-    }
-
-    private void setAdvImg(final AdvBean bean) {
-        assert bean != null;
-        RequestOptions options = new RequestOptions()
-                .fitCenter()
-                .placeholder(R.mipmap.ic_launcher)
-                .transform(new RoundedCorners(8));
-        Glide.with(this)
-                .load(bean.getFile())
-                .apply(options)
-                .into(advImg);
-        advImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(AdvActivity.this, bean.getUrl(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
 }
