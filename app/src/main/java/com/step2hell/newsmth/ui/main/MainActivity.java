d4f1a53e4197a5923c492e199bd7f14301961e84
yangbo <yangbo@nexa-corp.com>
2017-10-09 17:48:47 +0800

    setup divider of NavigationView

--- app/src/main/java/com/step2hell/newsmth/ui/main/MainActivity.java
+++ app/src/main/java/com/step2hell/newsmth/ui/main/MainActivity.java
@@ -3,18 +3,28 @@ package com.step2hell.newsmth.ui.main;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenuPresenter;
+import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.step2hell.newsmth.R;
 import com.step2hell.newsmth.ui.BaseActivity;
 import com.step2hell.newsmth.ui.SettingsActivity;
+
+import java.lang.reflect.Field;
 
 /**
  * Todo: MVVM, Design main page.
@@ -46,6 +56,72 @@ public class MainActivity extends BaseActivity {
         mNavigationView = (NavigationView) findViewById(R.id.main_navigation);
         mNavigationView.setNavigationItemSelectedListener(new NavigationItemSelectedListener());
         mNavigationView.setItemIconTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorAccent)));
+        setupNavigationDivider();
+    }
+
+    /**
+     * 通过反射修改原生NavaigationView的divider（也可以不使用NavigationView，自己写个RecyclerView作为drawer）
+     */
+    private void setupNavigationDivider() {
+
+        /**
+         * https://github.com/android/platform_frameworks_support/blob/master/design/res/layout/design_navigation_item.xml
+         */
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.listPreferredItemPaddingLeft, typedValue, true);
+        int[] attribute = new int[]{android.R.attr.listPreferredItemPaddingLeft};
+        TypedArray array = obtainStyledAttributes(typedValue.resourceId, attribute);
+        final int mNavigationMenuItemViewPaddingLeft = array.getDimensionPixelSize(0 /* index */, -1 /* default size */);
+        array.recycle();
+
+        /**
+         * https://github.com/android/platform_frameworks_support/blob/master/design/src/android/support/design/internal/NavigationMenuItemView.java
+         * mIconSize = context.getResources().getDimensionPixelSize(R.dimen.design_navigation_icon_size);
+         */
+        final int mNavigationMenuIconSize = MainActivity.this.getResources().getDimensionPixelSize(android.support.design.R.dimen.design_navigation_icon_size);
+
+        /**
+         * https://github.com/android/platform_frameworks_support/blob/master/design/res/layout/design_navigation_menu_item.xml
+         */
+        final int mNavigationMenuIconPadding = MainActivity.this.getResources().getDimensionPixelSize(android.support.design.R.dimen.design_navigation_icon_padding);
+
+        try {
+            Field fieldByPresenter = mNavigationView.getClass().getDeclaredField("mPresenter");
+            fieldByPresenter.setAccessible(true);
+            NavigationMenuPresenter menuPresenter = (NavigationMenuPresenter) fieldByPresenter.get(mNavigationView);
+            Field fieldByMenuView = menuPresenter.getClass().getDeclaredField("mMenuView");
+            fieldByMenuView.setAccessible(true);
+            final NavigationMenuView mMenuView = (NavigationMenuView) fieldByMenuView.get(menuPresenter);
+            mMenuView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+                @Override
+                public void onChildViewAttachedToWindow(View view) {
+                    RecyclerView.ViewHolder viewHolder = mMenuView.getChildViewHolder(view);
+                    if (viewHolder != null && "SeparatorViewHolder".equals(viewHolder.getClass().getSimpleName()) && viewHolder.itemView != null) {
+                        if (viewHolder.itemView instanceof FrameLayout) {
+                            FrameLayout frameLayout = (FrameLayout) viewHolder.itemView;
+                            View line = frameLayout.getChildAt(0);
+//                            line.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorAccent)); // change color here, or reset background.
+                            ViewGroup.LayoutParams params = line.getLayoutParams();
+//                            params.height = 1; // change height here.
+                            /**
+                             * make divider alignLeft with normal menu title, so I compute the MarginStart.
+                             */
+                            ((FrameLayout.MarginLayoutParams) params).setMarginStart(mNavigationMenuItemViewPaddingLeft + mNavigationMenuIconSize + mNavigationMenuIconPadding);
+                            line.setLayoutParams(params);
+                        }
+                    }
+                }
+
+                @Override
+                public void onChildViewDetachedFromWindow(View view) {
+
+                }
+            });
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 
     /* The click listner for item of NavigationView */
