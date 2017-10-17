91f01ffc1181bd50b7f3db6b282d0274dd79b863
Bob <40080007@qq.com>
2017-10-18 00:47:54 +0800

    fix bug: wrong method onCreate()

--- app/src/main/java/com/step2hell/newsmth/ui/BaseActivity.java
+++ app/src/main/java/com/step2hell/newsmth/ui/BaseActivity.java
@@ -37,8 +37,8 @@ public class BaseActivity extends AppCompatActivity {
     };
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         registerReceiver(connectivityReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
