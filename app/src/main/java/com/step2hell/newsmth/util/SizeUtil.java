4dd06048aab344cb1d3cf10a042e3567b32608f5
yangbo <yangbo@nexa-corp.com>
2017-12-28 15:53:56 +0800

    optimize getDisplayMetrics of SizeUtil

--- app/src/main/java/com/step2hell/newsmth/util/SizeUtil.java
+++ app/src/main/java/com/step2hell/newsmth/util/SizeUtil.java
@@ -2,36 +2,44 @@ package com.step2hell.newsmth.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 
 public class SizeUtil {
 
-    public static int getScreenWidthPx(Activity activity) {
-        DisplayMetrics metrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    public static int getScreenWidthPx() {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return metrics.widthPixels;
     }
 
-    public static int getScreenHeightPx(Activity activity) {
-        DisplayMetrics metrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    public static int getScreenHeightPx() {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return metrics.heightPixels;
     }
 
     // 测量尺寸以建适配文件夹sw<N>dp
+    public static int getScreenWidthDp() {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        Log.d("SizeUtil", "widthPixels:" + metrics.widthPixels);
+        Log.d("SizeUtil", "density:" + metrics.density);
+        Log.d("SizeUtil", "densityDpi:" + metrics.densityDpi);
+        return Math.round(metrics.widthPixels / metrics.density);
+    }
+
     public static int getScreenWidthDp(Activity activity) {
         DisplayMetrics metrics = new DisplayMetrics();
         activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         Log.d("SizeUtil", "widthPixels:" + metrics.widthPixels);
         Log.d("SizeUtil", "density:" + metrics.density);
+        Log.d("SizeUtil", "densityDpi:" + metrics.densityDpi);
         return Math.round(metrics.widthPixels / metrics.density);
+//        return (int) (metrics.widthPixels / metrics.density + 0.5f);
     }
 
-    public static int getScreenHeightDp(Activity activity) {
-        DisplayMetrics metrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    public static int getScreenHeightDp() {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return Math.round(metrics.heightPixels / metrics.density);
     }
 
