1213cb69d816dbdd55ca5e07c60372abb3a672fc
yangbo <yangbo@nexa-corp.com>
2018-01-31 17:15:08 +0800

    add ImageUtil

---
+++ app/src/main/java/com/step2hell/newsmth/util/ImageUtil.java
@@ -0,0 +1,26 @@
+package com.step2hell.newsmth.util;
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.widget.ImageView;
+
+public class ImageUtil {
+    public static void setTint(Drawable drawable, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            DrawableCompat.setTint(drawable, color);
+        } else {
+            drawable.mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    public static void setTint(ImageView imageView, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            DrawableCompat.setTint(imageView.getDrawable(), color);
+        } else {
+            imageView.getDrawable().mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+}
